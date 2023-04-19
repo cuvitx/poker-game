02e1debec5bbc1685c8b62a6814724baab9e9b49
cuvitx <130497484+cuvitx@users.noreply.github.com>
2023-04-19 22:21:22 +0200

    Update poker.js

--- poker.js
+++ poker.js
@@ -1,73 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const submitAnswerButton = document.getElementById('submit-answer');
-    const userAnswerInput = document.getElementById('user-answer');
-    const result = document.getElementById('result');
-    const cardDisplay = document.querySelector('.card-display');
+  const cardDisplay = document.getElementById('card-display');
+  const userInputForm = document.getElementById('user-input-form');
+  const userInput = document.getElementById('user-input');
+  const result = document.getElementById('result');
 
-    // Générer et afficher une main de poker
-    function generatePokerHand() {
-        const hand = []; // Un tableau pour stocker les cartes de la main
-        for (let i = 0; i < 5; i++) {
-            const card = generateRandomCard(); // Fonction pour générer une carte aléatoire
-            hand.push(cardToString(card));
-            const cardElement = document.createElement('div');
-            cardElement.classList.add('card');
-            cardElement.textContent = cardToString(card);
-            cardDisplay.appendChild(cardElement);
-        }
-        return hand;
+  const pokerHand = generatePokerHand();
+  displayPokerHand(pokerHand);
+
+  userInputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userAnswer = parseInt(userInput.value, 10);
+    const correctAnswer = calculateHandsBeaten(pokerHand);
+
+    if (userAnswer === correctAnswer) {
+      result.textContent = 'Bravo ! Vous avez trouvé la bonne réponse !';
+    } else {
+      result.textContent = `Dommage... La bonne réponse était ${correctAnswer}.`;
     }
-
-    // Fonction pour générer une carte aléatoire
-    function generateRandomCard() {
-        const ranks = '23456789TJQKA';
-        const suits = 'cdhs';
-        return {
-            rank: ranks[Math.floor(Math.random() * ranks.length)],
-            suit: suits[Math.floor(Math.random() * suits.length)]
-        };
-    }
-
-    // Fonction pour convertir une carte en chaîne de caractères
-    function cardToString(card) {
-        return card.rank + card.suit;
-    }
-
-    // Calculer le nombre de mains qui battent la main donnée
-    function calculateHandsBeaten(hand) {
-        const handEvaluator = new PokerEvaluator();
-        const handRank = handEvaluator.getHandRank(hand);
-        const totalHands = 2598960; // Nombre total de mains de poker (combinaisons de 5 cartes parmi 52)
-        const handsBeaten = totalHands - handRank;
-        return handsBeaten;
-    }
-
-    // Vérifier la réponse de l'utilisateur
-    function checkAnswer() {
-        const userAnswer = parseInt(userAnswerInput.value);
-        if (isNaN(userAnswer)) {
-            result.textContent = "Veuillez saisir un nombre.";
-            return;
-        }
-
-        const correctAnswer = calculateHandsBeaten(pokerHand);
-
-if (userAnswer === correctAnswer) {
-    result.textContent = "Félicitations ! Votre réponse est correcte.";
-} else {
-    result.textContent = `Désolé, la réponse correcte est ${correctAnswer}.`;
-}
-
-// Efface l'entrée de l'utilisateur et réinitialise la main affichée
-userAnswerInput.value = "";
-cardDisplay.innerHTML = "";
-pokerHand = generatePokerHand();
-}
-
-// Générer et afficher une main de poker initiale
-let pokerHand = generatePokerHand();
-
-// Ajouter un gestionnaire d'événement pour le bouton de soumission de réponse
-submitAnswerButton.addEventListener('click', checkAnswer);
+  });
 });
 
+function generatePokerHand() {
+  // Votre fonction pour générer une main de poker
+}
+
+function displayPokerHand(hand) {
+  hand.forEach(card => {
+    const cardImage = document.createElement('img');
+    const cardSuit = card.charAt(1).toUpperCase();
+    const cardValue = card.charAt(0).toUpperCase();
+    const suitName = suitToString(cardSuit);
+    const valueName = valueToString(cardValue);
+    cardImage.src = `cards/${valueName}_of_${suitName}.svg`;
+    cardImage.alt = `${valueName} of ${suitName}`;
+    cardImage.classList.add('card');
+    cardDisplay.appendChild(cardImage);
+  });
+}
+
+function suitToString(suit) {
+  switch (suit) {
+    case 'C':
+      return 'clubs';
+    case 'D':
+      return 'diamonds';
+    case 'H':
+      return 'hearts';
+    case 'S':
+      return 'spades';
+    default:
+      return '';
+  }
+}
+
+function valueToString(value) {
+  switch (value) {
+    case 'A':
+      return 'ace';
+    case 'K':
+      return 'king';
+    case 'Q':
+      return 'queen';
+    case 'J':
+      return 'jack';
+    default:
+      return value;
+  }
+}
+
+function calculateHandsBeaten(hand) {
+  // Votre fonction pour calculer le nombre de mains battues
+}
